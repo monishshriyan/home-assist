b32b4cc0556ab4bb02d506aac6535a18fba59bac
monishshriyan <93622640+monishshriyan@users.noreply.github.com>
2024-07-01 14:21:05 +0530

    added google_nav_bar dependency and revamped bottom nav bar

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -1,5 +1,6 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -13,30 +14,56 @@ class _BottomNavBarState extends State<BottomNavBar> {
 
   @override
   Widget build(BuildContext context) {
-    ThemeData(useMaterial3: true);
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.green,
         title: const Center(child: Text("HomeAssist")),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.blueGrey,
-        unselectedItemColor: const Color(0xFF526400),
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
-              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
-              label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
-              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
-              label: "Search"),
-          BottomNavigationBarItem(
-              icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
-              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
-              label: "Settings")
-        ],
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+        /* color: const Color.fromARGB(255, 13, 13, 13) */
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 13, 13, 13),
+          borderRadius: BorderRadius.all(Radius.circular(50)),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(37, 0, 0, 0),
+              spreadRadius: 0,
+              blurRadius: 10,
+              offset: Offset(0, -5), // Add this
+            ),
+          ],
+        ), // Add this),
+
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: GNav(
+              backgroundColor: Color.fromARGB(255, 13, 13, 13),
+              color: Color.fromARGB(246, 255, 255, 255),
+              activeColor: Color.fromARGB(243, 255, 255, 255),
+              tabBackgroundColor: Color.fromARGB(47, 18, 234, 65),
+              gap: 4,
+              duration: Duration(milliseconds: 200),
+              iconSize: 28,
+              padding: EdgeInsets.all(15),
+              haptic: true,
+              curve: Curves.easeInExpo,
+              tabs: [
+                GButton(
+                  icon: FluentSystemIcons.ic_fluent_home_regular,
+                  text: 'Home',
+                ),
+                GButton(
+                    icon: FluentSystemIcons.ic_fluent_search_regular,
+                    text: 'Search'),
+                GButton(
+                    icon: FluentSystemIcons.ic_fluent_person_regular,
+                    text: 'Account'),
+                GButton(
+                    icon: FluentSystemIcons.ic_fluent_settings_regular,
+                    text: 'Settings')
+              ]),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'HomeAssist',
-        theme: ThemeData(useMaterial3: false),
+        theme: ThemeData(useMaterial3: true),
         home: const BottomNavBar());
   }
 }
