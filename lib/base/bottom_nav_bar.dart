71d93ae635a060c64a2d752f46ed385a8a09977b
monishshriyan <93622640+monishshriyan@users.noreply.github.com>
2024-07-01 11:21:46 +0530

    Changed navbar icons

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -1,5 +1,5 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:iconoir_flutter/iconoir_flutter.dart' as iconoir;
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -9,21 +9,33 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
+  //list is iterated using index
+
   @override
   Widget build(BuildContext context) {
+    ThemeData(useMaterial3: true);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("HomeAssist"),
+        title: const Center(child: Text("HomeAssist")),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: const Color.fromARGB(255, 107, 181, 61),
+        selectedItemColor: Colors.blueGrey,
         unselectedItemColor: const Color(0xFF526400),
         showSelectedLabels: false,
+        showUnselectedLabels: false,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(
-              icon: Icon(Icons.search_off), label: "Search"),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings")
+              icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              label: "Search"),
+          BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              label: "Settings")
         ],
       ),
     );
